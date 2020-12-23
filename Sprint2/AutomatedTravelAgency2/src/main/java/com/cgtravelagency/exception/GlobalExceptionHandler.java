ccee009ed4244ec75bbbf56aa8ec937ff62024ee
Shrishti <shahishrishti@gmail.com>
2020-12-23 15:46:45 +0530

    GlobalExceptionHandler

---
+++ Sprint2/AutomatedTravelAgency2/src/main/java/com/cgtravelagency/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.cgtravelagency.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	
+	@Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
+            WebRequest request) {
+		List<String> errors = new ArrayList<>();
+		for(FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+			errors.add(fieldError.getField() + " - " + fieldError.getDefaultMessage());
+		}
+		for(ObjectError objectError : ex.getBindingResult().getGlobalErrors()) {
+			errors.add(objectError.getObjectName() + " - " + objectError.getDefaultMessage());
+		}
+		ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, 
+				ex.getLocalizedMessage(), errors);
+		return handleExceptionInternal(ex, errorMessage, headers, HttpStatus.BAD_REQUEST, request);
+	}
+}
