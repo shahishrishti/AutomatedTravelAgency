e88808f5e30d84f5c85859fbe05c5708bfa7e7c6
Shrishti <shahishrishti@gmail.com>
2020-12-26 18:07:26 +0530

    Loggers

--- Sprint2/AutomatedTravelAgency2/src/main/java/com/cgtravelagency/controller/VehicleTypeController.java
+++ Sprint2/AutomatedTravelAgency2/src/main/java/com/cgtravelagency/controller/VehicleTypeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
